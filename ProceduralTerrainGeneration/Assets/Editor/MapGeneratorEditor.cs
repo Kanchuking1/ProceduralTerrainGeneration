7c9d8cec4076f65ff932f279622bcd31822aca1a
Wibe <45963579+Kanchuking1@users.noreply.github.com>
2019-09-09 19:00:56 +0530

    CommitOne
    Able to Generate a 2D texture with Perlin Noise

---
+++ ProceduralTerrainGeneration/Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof(MapGenerator))]
+public class MapGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        MapGenerator MG = (MapGenerator)target;
+        if (DrawDefaultInspector())
+        {
+            if (MG.autoUpdate)
+            {
+                MG.GenerateMap();
+            }
+        }
+
+        if (GUILayout.Button("Generate"))
+        {
+            MG.GenerateMap();
+        }
+    }
+}
